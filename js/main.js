550132131011aec1c22c5d674d581f9859e4380f
Tai Phan <ptai7722@gmail.com>
2023-11-18 15:13:29 -0600

    Set up a test fetch to see the data we are working with, time to pseudo code

--- js/main.js
+++ js/main.js
@@ -1,1 +1,14 @@
 //This is the url template we will be fetching our information from https://world.openfoodfacts.net/api/v2/search?categories_tags_en=Orange Juice&countries_tags_en=United States&fields=product_name,nutrition_grades,brands,ingredients,nutriscore_grade,image_front_url,nova_group&sort_by=nutriscore_score
+//Lets first do a fetch on orange juice and see what we get.
+const URL = 'https://world.openfoodfacts.net/api/v2/search?categories_tags_en=Orange Juice&countries_tags_en=United States&fields=product_name,nutrition_grades,brands,ingredients,nutriscore_grade,image_front_url,nova_group&sort_by=nutriscore_score'
+fetch(URL)
+  .then(res => {
+    if (!res.ok) {
+      console.log('Failed Fetch')
+    }
+    console.log('Successful Fetch')
+    return res.json();
+    })
+  .then(data => data.products[0])
+  .then(product => console.log(product))
+  .catch(err => console.log(err));
