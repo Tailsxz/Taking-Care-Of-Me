1d93584a323176fabc11ee6ac93be160c371cf64
Tai Phan <ptai7722@gmail.com>
2023-11-18 16:17:44 -0600

    Set default values for when certain properties don"t exist, need to create a function to reset the innerHTML of the cards every new category search

--- js/main.js
+++ js/main.js
@@ -1,37 +1,45 @@
 //This is the url template we will be fetching our information from https://world.openfoodfacts.net/api/v2/search?categories_tags_en=Orange Juice&countries_tags_en=United States&fields=product_name,nutrition_grades,brands,ingredients,nutriscore_grade,image_front_url,nova_group&sort_by=nutriscore_score
 //Lets first do a fetch on orange juice and see what we get.
-const URL = 'https://world.openfoodfacts.net/api/v2/search?categories_tags_en=Orange Juice&countries_tags_en=United States&fields=product_name,nutrition_grades,brands,ingredients,nutriscore_grade,image_front_url,nova_group&sort_by=nutriscore_score'
-fetch(URL)
-  .then(res => {
-    if (!res.ok) {
-      console.log('Failed Fetch')
-    }
-    console.log('Successful Fetch')
-    return res.json();
-    })
-  .then(data => data.products[1])
-  .then(product => createCard(product))
-  .catch(err => console.log(err));
+// const URL = 'https://world.openfoodfacts.net/api/v2/search?categories_tags_en=Orange Juice&countries_tags_en=United States&fields=product_name,nutrition_grades,brands,ingredients,nutriscore_grade,image_front_url,nova_group&sort_by=nutriscore_score'
+// fetch(URL)
+//   .then(res => {
+//     if (!res.ok) {
+//       console.log('Failed Fetch')
+//     }
+//     console.log('Successful Fetch')
+//     return res.json();
+//     })
+//   .then(data => data.products)
+//   .then(products => products.forEach(product => createCard(product)))
+//   .catch(err => console.log(err));
 
 //Now that we can see the data for one product, lets start pseudocoding what we have to do.
 //We know we will recieve an array of object products, with the object containing all the related data. We want to seperate each piece of info we want to be stored in a variable to be able to use template literals to print each card to the dom for each product. Lets try to print one card to the dom for the first product for now.
 
 //Lets grab all the dom elements we need for this project, storing them globally in a variable
-const category = document.querySelector('.input_category');
-const searchButton = document.querySelector('button_search');
+const currentCategory = document.querySelector('.input_category');
+const searchButton = document.querySelector('.button_search');
 //This is the section we will be creating the cards in.
 const cardsSection = document.querySelector('.content_cards');
 
 function createCard(obj) {
-  const brand = obj.brands;
   const product = obj.product_name;
-  const imageUrl = obj.image_front_url || 'assets/alt-image.jpg';
-  //Ingredients will be given as an array
-  const ingredients = obj.ingredients;
-  const testIngredient = ingredients[0].id
-  const novaScore = obj.nova_group;
-  const nutriScore = obj.nutriscore_grade;
-  console.log(nutriScore);
+  let brand = obj.brands;
+  let imageUrl = obj.image_front_url;
+  console.log(imageUrl);
+  //Ingredients will be given as an array of objects, we are mapping these ingredients to their id property, and joining them to form the list of ingredients.
+  let ingredients;
+  if (ingredients in obj) {
+  ingredients = obj.ingredients.map(e => e.id.replace('en:', '')).join(', ');
+  }
+  console.log(ingredients)
+  let novaScore = obj.nova_group;
+  let nutriScore = obj.nutriscore_grade;
+  //Setting default values for properties that were not found
+  brand = brand || 'No brand specified';
+  imageUrl = imageUrl || 'assets/alt-image.jpg';
+  novaScore = obj.nova_group || 'No NOVA Score provided';
+  nutriScore = nutriScore || 'No Nutri-Score provided';
   const cardElement = document.createElement('section');
   cardElement.classList.add('card');
   cardElement.innerHTML = `<h2 class="card_brand">${brand}</h2>
@@ -41,13 +49,43 @@ function createCard(obj) {
     <span class="product_nutri-score">${nutriScore}
     </span>
     <span class="product_NOVA-score">${novaScore}</span>
-    <section class="product_ingredients">
-      <h3>Ingredients</h3>
-      <ul class="product_ingredients">
-        <li>${testIngredient}</li>
-      </ul>
-    </section>
+    ${ingredients ? `<section class="product_ingredients">
+    <h3>Ingredients</h3>
+    <p class="product_ingredients">${ingredients}
+    </p>
+  </section>` : ''}
   </section>`
   cardsSection.appendChild(cardElement);
 }
 //We have our card element creator, now lets see if we can do it for each item in the array.
+
+//Now that both the fetch and createCards() function is working successfully in printing the cards to the dom from the products result, we can encapsulate the fetch in a function to be called on the click event of the search button
+
+function fetchProducts() {
+  const category = currentCategory.value;
+  const url = `https://world.openfoodfacts.net/api/v2/search?categories_tags_en=${category}&countries_tags_en=United States&fields=product_name,nutrition_grades,brands,ingredients,nutriscore_grade,image_front_url,nova_group&sort_by=nutriscore_score`
+  fetch(url)
+    .then(res => {
+      if (!res.ok) {
+        console.log('Failed Fetch')
+      }
+      console.log('Successful Fetch')
+      return res.json();
+      })
+    .then(data => data.products)
+    .then(products => {
+      console.log(products);
+      products.forEach(product => createCard(product))
+    })
+    .catch(err => console.log(err));
+}
+
+//Function to initialize an event listener, with the parameters of the element we want to add a listener to, and the function to bind to the event.
+function initEventListener(element, func) {
+  element.addEventListener('click', func)
+}
+
+initEventListener(searchButton, (e) => {
+  e.preventDefault;
+  fetchProducts();
+});
