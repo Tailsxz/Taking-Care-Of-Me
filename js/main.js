9c8f173cd2b0346f4c3c1e200662778f7e522cb5
Tai Phan <ptai7722@gmail.com>
2023-11-20 17:49:42 -0600

    added an apply background function to set the Nutri-scores and NOVA scores to their appropriate background color :D

--- js/main.js
+++ js/main.js
@@ -15,6 +15,12 @@
 
 //Now that we can see the data for one product, lets start pseudocoding what we have to do.
 //We know we will recieve an array of object products, with the object containing all the related data. We want to seperate each piece of info we want to be stored in a variable to be able to use template literals to print each card to the dom for each product. Lets try to print one card to the dom for the first product for now.
+//Score Colors
+const RED = '#ff3900'
+const GREEN = '#00ba00';
+const YELLOW = '#ffcd00';
+const ORANGE = '#ff8400';
+const DARK_GREEN = '#008331';
 
 //Lets grab all the dom elements we need for this project, storing them globally in a variable
 const currentCategory = document.querySelector('.input_category');
@@ -23,7 +29,7 @@ const searchButton = document.querySelector('.button_search');
 const cardsSection = document.querySelector('.content_cards');
 
 //Function to dynamically create cards for each product
-function createCard(obj) {
+function createCard(obj, index) {
   const product = obj.product_name;
   let brand = obj.brands;
   let imageUrl = obj.image_front_url;
@@ -36,8 +42,8 @@ function createCard(obj) {
   //Setting default values for properties that were not found
   brand = brand || 'No brand specified';
   imageUrl = imageUrl || 'assets/alt-image.jpg';
-  novaScore = obj.nova_group || 'No NOVA Score provided';
-  nutriScore = nutriScore || 'No Nutri-Score provided';
+  novaScore = obj.nova_group || '?';
+  nutriScore = nutriScore || '?';
 
   //Mapping ingredients to their ids and joining them to form a list of ingredients.
   if (ingredients != undefined) {
@@ -49,11 +55,16 @@ function createCard(obj) {
   <p class="card_product | fs-small">${product}</p>
   <img src="${imageUrl}" alt="" class="card_image">
   <section class="card_info">
-    <h3 class="fs-xsmall">Nutri-Score</h3>
-    <span class="product_nutri-score fs-small">${nutriScore}
-    </span>
-    <h3 class="fs-xsmall">NOVA</h3>
-    <span class="product_NOVA-score fs-small">${novaScore}</span>
+    <section class="card_info-scores">
+      <div class="container_nutri">
+        <h3 class="fs-xsmall">Nutri-Score</h3>
+        <span class="product_nutri-score nutri${index} | fs-small">${nutriScore}</span>
+      </div>
+      <div class="container_nova">
+        <h3 class="fs-xsmall">NOVA</h3>
+        <span class="product_nova-score nova${index} | fs-small">${novaScore}</span>
+      </div>
+    </section>
     ${ingredients ? `<section class="product_ingredients">
     <h3 class="fs-medium">Ingredients</h3>
     <p class="product_ingredients | fs-small">${ingredients}
@@ -61,7 +72,15 @@ function createCard(obj) {
   </section>` : ''}
   </section>`
 
+  
   cardsSection.appendChild(cardElement);
+  //grabbing the newly created Nutri-score and NOVA scores
+  let nutri_score = document.querySelector(`.nutri${index}`);
+  let nova_score = document.querySelector(`.nova${index}`);
+  console.log(nutri_score, nova_score);
+  //calling the function to apply the background color for the scores
+  applyBackground(nutri_score, nutriScore);
+  applyBackground(nova_score, novaScore);
 }
 //We have our card element creator, now lets see if we can do it for each item in the array.
 
@@ -90,7 +109,7 @@ function fetchProducts() {
       })
     .then(data => data.products)
     .then(products => {
-      products.forEach(product => createCard(product))
+      products.forEach((product, index) => createCard(product, index))
     })
     .catch(err => console.log(err));
 }
@@ -107,3 +126,37 @@ function initEventListener(element, func) {
 initEventListener(searchButton, (e) => {
   fetchProducts();
 });
+
+//Function to set background on nutriscores and novascores
+
+function applyBackground(element, score) {
+  switch (score){
+  case 'A':
+    element.style.background = DARK_GREEN;
+    break;
+  case 'B':
+    element.style.background = GREEN;
+    break;
+  case 'C':
+    element.style.background = YELLOW;
+    break;
+  case 'D':
+    element.style.background = ORANGE;
+    break;
+  case 'E':
+    element.style.background = RED;
+    break;    
+  case   1:
+    element.style.background = GREEN;
+    break;
+  case   2:
+    element.style.background = YELLOW;
+    break;
+  case   3:
+    element.style.background = ORANGE;
+    break;
+  case   4:
+    element.style.background = RED;
+    break;
+  }
+}
